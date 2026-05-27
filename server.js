53dcf0dec250fb1f7c718ec452d818f8abe67b26
Leidiane Silva <ttechgirl@outlook.com>
2026-05-27 09:04:48 -0300

    Configurando a rota da API

--- server.js
+++ server.js
@@ -109,7 +109,7 @@ app.use((req, res) => {
 });
 
 // SPA fallback: serve index.html for non-API routes
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
